70eb70f7161f36b8b2ea6f76c078e65bc6b327c8
ArdianChambera <ardiancambera@gmail.com>
2021-04-16 21:16:11 +0800

    Quis akhir materi

---
+++ Inheritance/src/com/dicoding/javafundamental/inheritance/quis.java
@@ -0,0 +1,12 @@
+package com.dicoding.javafundamental.inheritance;
+
+public class quis {
+    public static void main(String[] args) {
+        String s1 = "geeksquiz";
+        String s2 = "geeksquiz";
+        System.out.println("s1 == s2 is:" + s1 == s2);
+
+        // Karena menggunakan operator '==',
+        // artinya membandingkan alamat di memori bukan nilainya.
+    }
+}
