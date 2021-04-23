6e912998374eef527f59613d780526609b46859c
ArdianChambera <ardiancambera@gmail.com>
2021-04-23 10:38:47 +0800

    Class

---
+++ Class/src/com/dicoding/javafundamental/kelas/NoArgumentConstructor.java
@@ -0,0 +1,10 @@
+package com.dicoding.javafundamental.kelas;
+
+public class NoArgumentConstructor {
+    public class KucingAnggora {
+
+        //ini adalah contoh no-argument constructor
+        public KucingAnggora() {
+        }
+    }
+}
